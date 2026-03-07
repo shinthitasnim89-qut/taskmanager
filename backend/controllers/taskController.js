bb591441d8941b2e182cadb92322c8d1c44c2ab0
Himi <s.himi@qut.edu.au>
2026-03-07 21:05:48 +1000

    taskController.js has been corrected

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,12 +1,12 @@
 //Read
 const Task = require('../models/Task');  
-const getTasks = async (req, res) => {  
+const getTasks = async (req, res) => {
 try {  
 const tasks = await Task.find({ userId: req.user.id });  
 res.json(tasks);  
 } catch (error) {  
 res.status(500).json({ message: error.message });
-  
+}
 }; 
 
 //Add
